4c5550413196d365aa9b5da0cf1141a672ffa065
Cooper F. Le <cooperfle@utdallas.edu>
2017-10-22 01:30:46 -0500

    Add info

--- db.js
+++ db.js
@@ -16,10 +16,16 @@ con.query("SELECT * FROM hacktober.invoices WHERE approved=0", (err, rows) => {
     rows.forEach( (row) => {
       const personID = row.personID;
       var html = "";
+      html = html + `<button onclick="accordion('drop${row.invoiceID}')" class="w3-btn w3-block w3-white w3-left-align">`
       html = html + "<li>";
       html = html + '<img class="avatar" src="' + users[personID].avatar + '" alt="logo" style="height:10%" align="left">';
       html = html + '<h4 class="text">' + users[personID].name + '</h4>';
       html = html + '<p class="text">' + row.timestamp + '</p> ';
+      html = html + '<div id="drop' + row.invoiceID + '" class="w3-container w3-hide text">';
+      html = html + '<h4 class="text">Event: ' + row.event + '</h4>';
+      html = html + `<h5 class="text">Receipt: <a href=${row.image} target="_blank">Image</a>`;
+      html = html + `<h5 class="text">Amount: $${row.amount}</h5>`;
+      html = html + `<h5 class="text">Notes: ${row.notes}</h5>`;
       html = html + "</li>";
       transactions = transactions + html;
       if(document.getElementById("transactions"))
@@ -32,31 +38,14 @@ function getUsers(){
   return users;
 }
 /*
-<ul class="w3-ul">
-                <li>
-                     <img class="avatar" src="https://pbs.twimg.com/profile_images/883032684284452864/pFLc_D2c_400x400.jpg" alt="logo" style="height:10%" align="left"> 
-                     <h4 class="text">Jason Waye</h4>
-                     <p id="time" class="text">15m ago</p>
-                </li>
-                <li>
-                     <img class="avatar" src="https://pbs.twimg.com/profile_images/883032684284452864/pFLc_D2c_400x400.jpg" alt="logo" style="height:10%" align="left"> 
-                     <h4 class="text">Cooper Le</h4>
-                     <p class="text">4h ago</p>
-                </li>
-                <li>
-                     <img class="avatar" src="https://pbs.twimg.com/profile_images/883032684284452864/pFLc_D2c_400x400.jpg" alt="logo" style="height:10%" align="left"> 
-                     <h4 class="text">Alyssa Tichenor</h4>
-                     <p class="text">16h ago</p>
-                </li>
-                <li>
-                     <img class="avatar" src="https://pbs.twimg.com/profile_images/883032684284452864/pFLc_D2c_400x400.jpg" alt="logo" style="height:10%" align="left"> 
-                     <h4 class="text">Sabrina Banh</h4>
-                     <p class="text">2d ago</p>
-                </li>
-                <li>
-                     <img class="avatar" src="https://pbs.twimg.com/profile_images/883032684284452864/pFLc_D2c_400x400.jpg" alt="logo" style="height:10%" align="left"> 
-                     <h4 class="text">Megan Chan</h4>
-                     <p class="text">4d ago</p>
-                </li>
-            </ul> 
+<li>
+      <img class="avatar" src="https://pbs.twimg.com/profile_images/883032684284452864/pFLc_D2c_400x400.jpg" alt="logo" style="height:10%" align="left"> 
+      <h4 class="text">Jason Waye</h4>
+      <p id="time" class="text">15m ago</p>
+      <div id="drop4" class="w3-container w3-hide">
+        <p>Pending Requests: 5</p>
+        <p><u><font color = blue>Transaction History</font></u></p>
+        <p>Send Message</p>
+    </div>
+</li>
 */
