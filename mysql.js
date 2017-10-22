385b9a0728d240e10b94a64745dc81a2a79d31c7
Cooper F. Le <cooperfle@utdallas.edu>
2017-10-21 23:00:32 -0500

    Add some mysql logic

--- mysql.js
+++ mysql.js
@@ -11,6 +11,23 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
+getOpenTransactions();
 function getConnection() {
   return con;
+}
+
+function getOpenTransactions() {
+  con.query("SELECT * FROM hacktober.invoices WHERE approved=0", (err, rows) => {
+    if(err) return err.stack;
+
+    rows.forEach( (row) => {
+      console.log(`${row.event} cost $${row.amount}.`);
+      con.query(`SELECT * from hacktober.users WHERE personID=${row.personID}`, (err,users) => {
+        users.forEach((user) => {
+          console.log(`${user.name} has a balance of $${user.balance}.`);
+        });
+      });
+    });
+  });
+
 }
